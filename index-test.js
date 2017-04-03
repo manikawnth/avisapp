329917049cf47b07a74090eab84446297f71783a
manikawnth <manikawnth@gmail.com>
2017-04-03 14:56:33 -0400

    New end point

--- index-test.js
+++ index-test.js
@@ -1,45 +1,100 @@
 const alexa = require('alexa-app');
 const rp = require('request-promise');
-const li = require('./location-inquiry');
+//const li = require('./location-inquiry');
 
 const skill = new alexa.app("AvisApp");
+const END_POINT = 'https://mqa.avis.com/mobileapi/v2/request';
+const SERVICE = 'FindLocationsByKeyword';
 
 skill.launch(function (request, response) {
-  const shout = "Welcome to Avis Info Services! What would you like to know?";
-  response.say(shout).shouldEndSession(false);
+    const shout = "Welcome to Avis Info Services! What would you like to know?";
+    response.say(shout).shouldEndSession(false);
 });
 
 skill.intent("LocationInquiryIntent", {
-  "slots": { "location": "AMAZON.US_CITY" },
+    "slots": { "location": "AMAZON.US_CITY" },
 
-  "utterances": [
-    "what are some rental counters near {-|location} ",
-    "car rental locations near {-|location}",
-    "any rental locations at {-|location}",
-    "address of nearest locations {-|location}",
-    "address of nearest stations {-|location}",
-    "need nearest locations {-|location}",
-    "looking for closest locations of {-|location}"
-  ]
+    "utterances": [
+        "what are some rental counters near {-|location} ",
+        "car rental locations near {-|location}",
+        "any rental locations at {-|location}",
+        "address of nearest locations {-|location}",
+        "address of nearest stations {-|location}",
+        "need nearest locations {-|location}",
+        "looking for closest locations of {-|location}"
+    ]
 },
-  function (request, response) {
-    var location = request.slot('location');
+    function (req, response) {
+        console.log("intent invoked");
+        var location = req.slot('location');
+        //location = location.split(' ').join('%20');
 
-    const breakTime = `<break time="1s" />`;
-    return li(location)
-      .then(function (suggestions) {
-        console.log("came to mains");
-        let text = '';
-        for (let each_location of suggestions) {
-          text = text + each_location + breakTime;
+        const breakTime = `<break time="1s" />`;
+        
+        let request = {
+            "requestHeader": {
+                "clientLanguageCode": "EN",
+                "clientCountryCode": "US",
+                "apiVersion": "1",
+                "brand": "AVIS",
+                "clientType": "CHATBOT",
+                "deviceDetails": {
+                    "platform": "SCH-I545",
+                    "manufacturer": "samsung",
+                    "operatingSystem": "android",
+                    "osVersion": "5.0.1",
+                    "appVersion": "7.3.3"
+                }
+            },
+            "keyword": "hi",
+            "vehicleTypesOffered": "PASSENGER"
         }
-        response.say(text).send();
-      })
-      .catch(function(err){
-        console.log(err);
-        response.say("Some unknown error occured").send();
-      })
-  }
+
+        request['keyword'] = location;
+        let options = {
+            method: 'POST',
+            uri: END_POINT,
+            body: request,
+            json: true,
+            headers: {
+                'content-type': 'application/json',
+                'Service': 'FindLocationsByKeyword'
+            }
+        };
+
+        return rp(options)
+            .then(function (resp) {
+                console.log("response received");
+                if (resp.locations) {
+                    const locations = resp.locations;
+                    const loc_len = resp.locations.length > 3 ? 3 : resp.locations.length;
+                    const suggestions = [];
+                    for (let i = 0; i < loc_len; i++) {
+                        if (locations[i].name) {
+                            suggestions.push(locations[i].name);
+                        }
+                        else {
+                            suggestions.push(locations[i].code);
+                        }
+                    }
+
+                    let text = '';
+                    for (let each_location of suggestions) {
+                        text = text + each_location + breakTime;
+                    }
+                    response.say(text).send();
+                }
+                else {
+                    response.say("No matching locations found").send();
+                }
+            })
+            .catch(function (err) {
+                console.log("Some error");
+                console.log(err);
+                response.say("Some unknown error occured").send();
+            })
+
+    }
 );
 //console.log(skill.schema());
 //console.log(skill.utterances());
--- location-inquiry.js
+++ location-inquiry.js
@@ -1,10 +1,13 @@
 const rp = require('request-promise');
 
+/* Old end-point code
 const END_POINT = 'https://mqa.avis.com/mobileapi/v2/request';
 const SERVICE = 'FindLocationsByKeyword';
+*/
 
-function LocationInquiry(location){
+function LocationInquiry(location) {
 
+    /* Old end-point code
     let request = {
         "requestHeader": {
             "clientLanguageCode": "EN",
@@ -13,59 +16,79 @@ function LocationInquiry(location){
             "brand": "AVIS",
             "clientType": "CHATBOT",
             "deviceDetails": {
-            "platform": "SCH-I545",
-            "manufacturer": "samsung",
-            "operatingSystem": "android",
-            "osVersion": "5.0.1",
-            "appVersion": "7.3.3"
+                "platform": "SCH-I545",
+                "manufacturer": "samsung",
+                "operatingSystem": "android",
+                "osVersion": "5.0.1",
+                "appVersion": "7.3.3"
             }
         },
         "keyword": "hi",
-        "vehicleTypesOffered":"PASSENGER"
+        "vehicleTypesOffered": "PASSENGER"
     }
-    
+
     request['keyword'] = location;
 
     let options = {
         method: 'POST',
         uri: END_POINT,
         body: request,
-        json : true,
-        headers : {
-            'content-type' : 'application/json',
-            'Service' : 'FindLocationsByKeyword'
+        json: true,
+        headers: {
+            'content-type': 'application/json',
+            'Service': 'FindLocationsByKeyword'
         }
     };
-    
-    return rp(options)
-    .then(function(resp){
-        console.log("came from api");
-        const promise = new Promise(function(resolve,reject){
-            if(resp.locations){
-                const locations = resp.locations;
+    */
+
+    //New end point code starts
+    const inlocation = location.split(' ').join('%20')
+    const END_POINT = `https://qaservices.carrental.com/mobileapi/avis/qa/v2/locations?keyword=${inlocation}`
+    let options = {
+        method: 'GET',
+        uri: END_POINT,
+        json: true,
+        headers: {
+            'content-type': 'application/json',
+            'brand' : 'Avis',
+            'clientCountryCode' : 'US',
+            'clientType' : 'MOBILEAPP'
+        }
+    };
+    //New end point code ends
+    const promise = new Promise((resolve, reject) => {
+        console.log("Creating promise");
+        rp(options)
+        .then((resp) => {
+            if (resp.locations && resp.responseInfo.success) {
+                let locations = resp.locations;
+                //Very ugly temporary fix. API needs to be changed
+                if((!locations[0].code) || (!locations[0].name)){
+                    locations.shift();
+                }
                 const loc_len = resp.locations.length > 3 ? 3 : resp.locations.length;
                 const suggestions = [];
-                for (let i=0; i<loc_len; i++){
-                    if (locations[i].name){
+                for (let i = 0; i < loc_len; i++) {
+                    if (locations[i].name) {
                         suggestions.push(locations[i].name);
                     }
-                    else{
+                    else {
                         suggestions.push(locations[i].code);
                     }
                 }
                 console.log(suggestions);
                 resolve(suggestions);
             }
-            else{
-                reject("Something wrong");
+            else {
+                reject("Locations not present");
             }
+        }, (err) => {
+            console.log("Error in location inquiry");
+            reject(err);
         });
-        console.log("came from api sending promise");
-        return promise;
     })
-    .catch(function(err){
-        throw err;
-    })
+    console.log("Returning promise");
+    return promise;
 
 }
 
